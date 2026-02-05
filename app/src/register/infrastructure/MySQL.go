2f4087761a150b8b8e300424474191020a80aa55
BryanChanona <233299@ids.upchiapas.edu.mx>
2026-02-05 03:45:18 -0600

    feat: Agregar funcionalidad de crear usuario

---
+++ app/src/register/infrastructure/MySQL.go
@@ -0,0 +1,25 @@
+package infrastructure
+
+import (
+	"database/sql"
+
+	"github.com/BryanChanona/api_chombi.git/app/src/register/domain/entities"
+)
+
+type MySQL struct{
+	db *sql.DB
+}
+
+func NewMySQL(db *sql.DB)*MySQL{
+	return &MySQL{db: db}
+}
+
+func (mysql *MySQL) Save(user entities.User) error {
+	query := "INSERT INTO user (name, lastName, email, passwordHash) VALUES (?, ?, ?, ?)"
+	_, err := mysql.db.Exec(query, user.Name, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
